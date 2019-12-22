787ff1ed523ecb92402b8d82676412b3d2e697e4
gfmen <gfmen@DESKTOP-BO6TOSR>
2019-12-22 12:58:08 -0600

    Cambios

---
+++ Sycom/Sycom.BL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sycom.BL
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(): base("SycomDB")
+        {
+
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
--- Sycom/Sycom.BL/ProductosBL.cs
+++ Sycom/Sycom.BL/ProductosBL.cs
@@ -8,29 +8,15 @@ namespace Sycom.BL
 {
     public class ProductosBL
     {
-        public List<Producto> ObtenerProductos()
+        Contexto _contexto;
+        public ProductosBL()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Laptop";
-            producto1.Precio = 15600;
+            _contexto = new Contexto();
+        }
 
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Computadora";
-            producto2.Precio = 13500;
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Impresora";
-            producto3.Precio = 2450;
-
-            var ListadeProductos = new List<Producto>();
-            ListadeProductos.Add(producto1);
-            ListadeProductos.Add(producto2);
-            ListadeProductos.Add(producto3);
-
-            return ListadeProductos;
+        public List<Producto> ObtenerProductos()
+        {            
+            return _contexto.Productos.ToList(); ;
         }
     }
 }
